69f292ac350dccc6440907ca705bbddb1771d461
analu5 <61354622+analu5@users.noreply.github.com>
2020-03-28 09:25:48 -0600

    Add files via upload

---
+++ ejecicioEnClase.py
@@ -0,0 +1,21 @@
+#primerNumero = 9
+#segundoNumero = 10
+
+#print (primerNumero+segundoNumero)
+
+#insiso 3
+n1 = float(input("Ingrese el primer Numero"))
+n2 = float(input("Ingrese el segundo Numero"))
+n3 = float(input("Ingrese el tercer Numero"))
+
+if n2 < n1 >n3:
+    print("El numero mayor es el primer numero. Numero", n1)
+elif n1 < n2 > n3:
+    print("El numero mayor es el segundo numero. Numero", n2)
+elif n1 < n3 > n2:
+    print("El numero mayor es el tercer numero. Numero", n3)
+
+git init
+git add .
+git commit -m "first commit"
+
